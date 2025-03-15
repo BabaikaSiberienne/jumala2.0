8735e6aab114d917704ff4d1d8b265f2b4927a51
Evgeniya <evgeniyaShalomeenko@yandex.ru>
2025-03-15 13:45:21 +0300

    tour pics added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {
     openModal,
     onClose,
 } from './components/modals';
+import { cardsFirstPages } from './scripts/cards.js'
 
 //переменные
 //кнопка показа сертификатов
@@ -12,6 +13,15 @@ const certificatesButton = document.querySelector('.main__certificates-button');
 const certificateModal = document.querySelector('.modal');
 //кнопка закрытия
 const closeButton = document.querySelector('.modal__close');
+
+//темплейт
+const tourTemplate = document.querySelector('#main__tour-template').content;
+
+//дом-узел, где картинки туров
+const tourContainer = document.querySelector('.main__tours-list');
+
+//одна картинка из шаблона
+let tourCard = tourTemplate.querySelector('.main__one-tour');
 
 //функции
 //открытие модалки с сертификатами
@@ -29,6 +39,27 @@ certificatesButton.addEventListener('click', openCertModal);
 
 //кнопка закрытия модалки
 closeButton.addEventListener('click', closeCertModal);
+
+//выведение карточек туров на страницу
+function showTour(data) {
+    const tourItem = tourCard.cloneNode(true)
+    const tourItemImage = tourItem.querySelector('.main__tour-image');
+    console.log(tourItemImage)
+    tourItemImage.src = data.link;
+    tourItemImage.alt = data.alt;
+    
+    const tourItemTitle = tourItem.querySelector('.main__tour-name');
+    console.log(tourItemTitle)
+    tourItemTitle.textContent = data.title
+    console.log(tourItem)
+    tourCard = tourItem
+    return tourCard 
+}
+
+cardsFirstPages.forEach(function(item) {
+    showTour(item);
+    tourContainer.prepend(tourCard);
+})
 
 // @todo: DOM узлы
 
